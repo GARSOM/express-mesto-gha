47d4005ebb2f922bb56993758db05246aae16b0b
Garik Kamalyan <garsom@mail.ru>
2023-09-15 10:06:06 +0300

    14

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(cookieParser());
 
